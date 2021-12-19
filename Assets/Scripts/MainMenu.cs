7b31b2c9f976cd0a6a6819cf97f8c2f45b5ccbb6
YawiDev <87639748+YawiDev@users.noreply.github.com>
2021-12-19 15:16:02 +1300

    Added Main Menu UI Elements
    Added simple UI to the main starting menu, with a MainMenu script that handles the on-click events for the buttons by calling the corresponding methods.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame () {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Quit () {
+        #if UNITY_EDITOR
+            EditorApplication.ExitPlaymode();
+        #else
+            Application.Quit();
+        #endif
+    }
+}
